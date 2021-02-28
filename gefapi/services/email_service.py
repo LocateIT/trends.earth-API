d672a22625b2181d4c61c4cc295713796033819e
Grace Amondi <ecofluff990@gmail.com>
2021-02-28 12:39:56 +0300

    remove external ports docker

--- gefapi/services/email_service.py
+++ gefapi/services/email_service.py
@@ -1,5 +1,3 @@
-"""SCRIPT SERVICE"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,49 +5,50 @@ from __future__ import print_function
 import logging
 
 from gefapi.errors import EmailError
-# from sparkpost import SparkPost
-from flask import Flask
-from flask_mail import Mail, Message
-import os
+from sparkpost import SparkPost
 
-app = Flask(__name__)
+#from flask import Flask
+# from flask_mail import Mail, Message
+#import os
 
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['EMAIL_USER'],
-    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
-}
+#app = Flask(__name__)
 
-app.config.update(mail_settings)
-mail = Mail(app)
+#mail_settings = {
+#    "MAIL_SERVER": 'smtp.gmail.com',
+#    "MAIL_PORT": 465,
+#    "MAIL_USE_TLS": False,
+#    "MAIL_USE_SSL": True,
+#    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+#    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+#}
+
+#app.config.update(mail_settings)                                                                                                                                                                                
+#mail = Mail(app)
 
 class EmailService(object):
     """MailService Class"""
-    
+
     @staticmethod
-    def send_html_email(recipients=[], html='', sender=app.config.get("MAIL_USERNAME"), subject='[MISLAND] Undefined Subject'):
+    def send_html_email(recipients=[], html='', from_email='info@promiseevents.co.ke', subject='[MISLAND] Undefined Subject'):
         try:
-            
-            with app.app_context():
-                response = Message(
-                    recipients = recipients,
-                    html = html,
-                    sender = sender,
-                    subject = subject
-                )
-                logging.info('{}'.format(response))
-                mail.send(response)
-            # sp = SparkPost()
-            # response = sp.transmissions.send(
-            #     recipients=recipients,
-            #     html=html,
-            #     from_email=from_email,
-            #     subject=subject
-            # )
-            # return response
+
+            #with app.app_context():
+            #    response = Message(
+            #        recipients = recipients,
+            #        html = html,
+            #        sender = sender,
+            #        subject = subject
+            #    )
+            #    logging.info('{}'.format(response))
+            #    mail.send(response)
+            sp = SparkPost()
+            response = sp.transmissions.send(
+                recipients=recipients,
+                html=html,
+                from_email=from_email,
+                subject=subject
+            )
+            return response
         except Exception as error:
             logging.error(error)
             raise EmailError(message=error)
